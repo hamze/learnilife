3e5ddd8a172be086a8ee19f248d7d9cf37aefdb5
Hamze <hamz.ghp@gmail.com>
2023-09-09 08:57:33 +0330

    first push

---
+++ learnilife/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from learnilife.users.models import User
+from learnilife.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
